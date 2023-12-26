d0e13720e151bc0e268d36febb420742c5678cbd
Mahnoor426 <122457858+Mahnoor426@users.noreply.github.com>
2023-12-26 23:13:22 +0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php require_once "user_login/controllerUserData.php"; 
 $active = basename($_SERVER['PHP_SELF'], ".php"); 
 $pageTitle = getPageTitle($active);
-include_once('dbconnection.php');
+use 'dbconnection.php';
 if(isset($_SESSION['is_Login'])){
 $email = $_SESSION['email'];
 $password = $_SESSION['password'];
@@ -69,8 +69,10 @@ function getPageTitle($active) {
     <link rel="stylesheet" href="css/swipe4.css">
       <!----===== Boxicons CSS ===== -->
       <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -88,7 +90,8 @@ function getPageTitle($active) {
   <nav class="navbar fixed-top navbar-expand-xl">
         <div class="container-fluid">
           <a class="navbar-brand" href="#"><img  src="image/assets/logo.png" width="80px" height="80px" style="margin-right:50px" /></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -101,12 +104,14 @@ function getPageTitle($active) {
               
               <?php 
                     if(isset($_SESSION['is_Login'])){
-                      echo '<a class="nav-link ' . (($active == "dashboard" || $active == "history" || $active == "feedback") ? 'active1' : '') . '" href="dashboard.php">Donor Corner</a>
+                      echo '<a class="nav-link ' . (($active == "dashboard" || $active == "history" || $active == "feedback") ? 'active1' : '') 
+                          . '" href="dashboard.php">Donor Corner</a>
                         <a href="user_login/logout-user.php"><button type="button" style="width:160px; height:40px" class="btn btn-light">Logout</button></a>';
                       }
       
                       else{
-                        echo'<a href="user_login/login-user.php"><button type="button" style="width:160px; height:40px" class="btn btn-light">Login / Register</button></a>';
+                        echo'<a href="user_login/login-user.php"><button type="button" style="width:160px; height:40px" 
+                        class="btn btn-light">Login / Register</button></a>';
                       }
               ?>
 
